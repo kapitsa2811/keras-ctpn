74dd25e4c8aa5dec4db902565851457c946d0620
kapitsa2811 <46664776+kapitsa2811@users.noreply.github.com>
2019-07-05 11:21:34 +0530

    Update models.py

--- ctpn/layers/models.py
+++ ctpn/layers/models.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
    File Name：     models
-   Description :  模型
+   Description :  模型 model
    Author :       mick.yi
    date：          2019/3/13
 """
@@ -17,22 +17,27 @@ from .text_proposals import TextProposal
 
 
 def ctpn_net(config, stage='train'):
-    # 网络构建
+    # 网络构建 Network construction
     # input_image = Input(batch_shape=(config.IMAGES_PER_GPU,) + config.IMAGE_SHAPE, name='input_image')
     # input_image_meta = Input(batch_shape=(config.IMAGES_PER_GPU, 12), name='input_image_meta')
     # gt_class_ids = Input(batch_shape=(config.IMAGES_PER_GPU, config.MAX_GT_INSTANCES, 2), name='gt_class_ids')
     # gt_boxes = Input(batch_shape=(config.IMAGES_PER_GPU, config.MAX_GT_INSTANCES, 5), name='gt_boxes')
+      
+    '''
+      measure change here is nw takes 4 input instead of 1 
+    '''
+    
     input_image = Input(shape=config.IMAGE_SHAPE, name='input_image')
     input_image_meta = Input(shape=(12,), name='input_image_meta')
     gt_class_ids = Input(shape=(config.MAX_GT_INSTANCES, 2), name='gt_class_ids')
     gt_boxes = Input(shape=(config.MAX_GT_INSTANCES, 5), name='gt_boxes')
 
-    # 预测
+    # 预测 prediction
     base_features = resnet50(input_image)
     num_anchors = len(config.ANCHORS_HEIGHT)
     predict_class_logits, predict_deltas, predict_side_deltas = ctpn(base_features, num_anchors, 64, 256)
 
-    # anchors生成
+    # anchors 生成 Generate
     anchors, valid_anchors_indices = CtpnAnchor(config.ANCHORS_HEIGHT, config.ANCHORS_WIDTH, config.NET_STRIDE,
                                                 name='gen_ctpn_anchors')(base_features)
 
@@ -43,7 +48,7 @@ def ctpn_net(config, stage='train'):
                              max_gt_num=config.MAX_GT_INSTANCES,
                              name='ctpn_target')([gt_boxes, gt_class_ids, anchors, valid_anchors_indices])
         deltas, class_ids, anchors_indices = targets[:3]
-        # 损失函数
+        # 损失函数 Loss function
         regress_loss = layers.Lambda(lambda x: ctpn_regress_loss(*x),
                                      name='ctpn_regress_loss')([predict_deltas, deltas, anchors_indices])
         side_loss = layers.Lambda(lambda x: side_regress_loss(*x),
@@ -68,7 +73,7 @@ def ctpn_net(config, stage='train'):
 
 def ctpn(base_features, num_anchors, rnn_units=128, fc_units=512):
     """
-    ctpn网络
+    ctpn 网络 The internet
     :param base_features: (B,H,W,C)
     :param num_anchors: anchors个数
     :param rnn_units:
@@ -76,7 +81,7 @@ def ctpn(base_features, num_anchors, rnn_units=128, fc_units=512):
     :return:
     """
     x = layers.Conv2D(512, kernel_size=(3, 3), padding='same', name='pre_fc')(base_features)  # [B,H,W,512]
-    # 沿着宽度方式做rnn
+    # 沿着宽度方式做  Do it along the width rnn
     rnn_forward = layers.TimeDistributed(layers.GRU(rnn_units, return_sequences=True, kernel_initializer='he_normal'),
                                          name='gru_forward')(x)
     rnn_backward = layers.TimeDistributed(
@@ -85,17 +90,17 @@ def ctpn(base_features, num_anchors, rnn_units=128, fc_units=512):
 
     rnn_output = layers.Concatenate(name='gru_concat')([rnn_forward, rnn_backward])  # (B,H,W,256)
 
-    # conv实现fc
+    # conv   achieve实现 fc
     fc_output = layers.Conv2D(fc_units, kernel_size=(1, 1), activation='relu', name='fc_output')(
         rnn_output)  # (B,H,W,512)
 
-    # 分类
+    # 分类 classification
     class_logits = layers.Conv2D(2 * num_anchors, kernel_size=(1, 1), name='cls')(fc_output)
     class_logits = layers.Reshape(target_shape=(-1, 2), name='cls_reshape')(class_logits)
-    # 中心点垂直坐标和高度回归
+    # 中心点垂直坐标和高度回归 Center point vertical coordinate and height 
     predict_deltas = layers.Conv2D(2 * num_anchors, kernel_size=(1, 1), name='deltas')(fc_output)
     predict_deltas = layers.Reshape(target_shape=(-1, 2), name='deltas_reshape')(predict_deltas)
-    # 侧边精调(只需要预测x偏移即可)
+    # 侧边精调(只需要预测x偏移即可) Side fine adjustment (just need to predict x offset)
     predict_side_deltas = layers.Conv2D(num_anchors, kernel_size=(1, 1), name='side_deltas')(fc_output)
     predict_side_deltas = layers.Reshape(target_shape=(-1, 1), name='side_deltas_reshape')(
         predict_side_deltas)
@@ -111,17 +116,17 @@ def get_layer(model, name):
 
 def compile(keras_model, config, loss_names=[]):
     """
-    编译模型，增加损失函数，L2正则化以
+    编译模型，增加损失函数，L2正则化以  compile the model, increase the loss function, L2 regularize to
     :param keras_model:
     :param config:
     :param loss_names: 损失函数列表
     :return:
     """
-    # 优化目标
+    # 优化目标 optimize the target
     optimizer = keras.optimizers.SGD(
         lr=config.LEARNING_RATE, momentum=config.LEARNING_MOMENTUM,
         clipnorm=config.GRADIENT_CLIP_NORM)
-    # 增加损失函数，首先清除之前的，防止重复
+    # 增加损失函数，首先清除之前的，防止重复 Increase the loss function, first clear the previous one, prevent duplication
     keras_model._losses = []
     keras_model._per_input_losses = {}
 
@@ -133,20 +138,20 @@ def compile(keras_model, config, loss_names=[]):
                 * config.LOSS_WEIGHTS.get(name, 1.))
         keras_model.add_loss(loss)
 
-    # 增加L2正则化
-    # 跳过批标准化层的 gamma 和 beta 权重
+    # 增加L2正则化 Increase L2 regularization
+    # 跳过批标准化层的 gamma 和 beta 权重  Skip the gamma and beta weights of the batch normalization layer
     reg_losses = [
         keras.regularizers.l2(config.WEIGHT_DECAY)(w) / tf.cast(tf.size(w), tf.float32)
         for w in keras_model.trainable_weights
         if 'gamma' not in w.name and 'beta' not in w.name]
     keras_model.add_loss(tf.add_n(reg_losses))
 
-    # 编译
+    # 编译 Compile
     keras_model.compile(
         optimizer=optimizer,
         loss=[None] * len(keras_model.outputs))  # 使用虚拟损失
 
-    # 为每个损失函数增加度量
+    # 为每个损失函数增加度量  Add a measure for each loss function
     for name in loss_names:
         if name in keras_model.metrics_names:
             continue
@@ -162,7 +167,7 @@ def compile(keras_model, config, loss_names=[]):
 
 def add_metrics(keras_model, metric_name_list, metric_tensor_list):
     """
-    增加度量
+    增加度量 Increase metric
     :param keras_model: 模型
     :param metric_name_list: 度量名称列表
     :param metric_tensor_list: 度量张量列表
